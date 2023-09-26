ce1e9c8e89594308f37e59fefb2a4a479b5d766a
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-09-26 21:03:24 +0800

    ✨ feat(index.ts):
    test server range support

--- src/index.ts
+++ src/index.ts
@@ -5,66 +5,106 @@ enum TEST_METHOD {
 	SELF = 'self',
 }
 
-interface IOptions {
+interface IDownOptions {
 	max: number;
 	testMethod: TEST_METHOD.HEAD | TEST_METHOD.SELF;
 }
 
-const defaultOptions: IOptions = {
+const defaultOptions: IDownOptions = {
 	max: 3,
 	testMethod: TEST_METHOD.HEAD,
 };
 
-// declare module 'axios' {
-// 	interface AxiosInstance {
-// 		down: any;
-// 	}
-// }
+declare module 'axios' {
+	interface AxiosInstance {
+		down: any;
+	}
+}
 
 // d.info.hns5j.com/100M.test
-export default function axiosMultiDown(axios: AxiosInstance, options: IOptions = defaultOptions): AxiosInstance {
-	const { max, testMethod }: IOptions = { ...defaultOptions, ...options };
+export default function axiosMultiDown(axios: AxiosInstance, options: IDownOptions = defaultOptions): AxiosInstance {
+	axios.down = async function <D = any>(axiosConfig: AxiosRequestConfig<D>) {
+		const downOptions: IDownOptions = { ...defaultOptions, ...options };
 
-	axios.down = async function <D = any>(config: AxiosRequestConfig<D>) {
-		// test 1 byte
-		const controller = new AbortController();
-		const headers = {
-			...config.headers,
-			Range: 'bytes=0-0',
-		};
+		const contentLength = await testRangeSupport(axios, downOptions, axiosConfig);
 
-		let contentLength;
+		console.log('contentLength', contentLength);
 
-		if (testMethod === TEST_METHOD.HEAD) {
-			const resp = await testByHead();
-		} else if (testMethod === TEST_METHOD.SELF) {
-			try {
-				axios({
-					...config,
-					headers,
-					signal: controller.signal,
-					responseType: 'stream',
-				}).then(resp => {
-					resp.data.on('data', (chunk: any) => {
-						const contentRange = resp.headers['content-range']; // bytes 0-0/104857607
-						if (chunk.length === 1 && contentRange) {
-							contentLength = contentRange.split('/')[1];
-							console.log('contentRange', contentRange, length);
-						}
-						controller.abort();
-					});
-				});
-			} catch (error) {
-				console.log('error', error);
-			}
+		// not support
+		if (!contentLength) {
+			return axios(axiosConfig);
+		} else {
 		}
 	};
 
 	return axios;
 }
 
-async function testByHead() {}
+type testContentLength = number | null;
 
-function testBySelf(axios: AxiosInstance) {}
+async function testRangeSupport<D>(axios: AxiosInstance, downOptions: IDownOptions, axiosConfig: AxiosRequestConfig<D>) {
+	const { testMethod } = downOptions;
+	const headers = {
+		...axiosConfig.headers,
+		Range: 'bytes=0-0',
+	};
+
+	const testAxiosConfig: AxiosRequestConfig = {
+		...axiosConfig,
+		headers,
+	};
+
+	let contentLength: testContentLength = null;
+
+	if (testMethod === TEST_METHOD.HEAD) {
+		contentLength = await testByHead(axios, testAxiosConfig);
+	} else {
+		contentLength = await testBySelf(axios, testAxiosConfig);
+	}
+
+	return contentLength;
+}
+
+function testByHead(axios: AxiosInstance, testAxiosConfig: AxiosRequestConfig): Promise<testContentLength> {
+	return new Promise(resolve => {
+		axios({ ...testAxiosConfig, method: TEST_METHOD.HEAD })
+			.then(resp => {
+				// head  -> body = empty
+				const contentRange = resp.headers['content-range']; // bytes 0-0/104857607
+				const contentLength: 1 | '1' | undefined = resp.headers['content-length'];
+				if (contentLength == 1 && contentRange) {
+					resolve(Number(contentRange.split('/')[1]));
+				} else {
+					resolve(null);
+				}
+			})
+			.catch(err => {
+				resolve(null);
+			});
+	});
+}
+
+function testBySelf(axios: AxiosInstance, testAxiosConfig: AxiosRequestConfig): Promise<testContentLength> {
+	return new Promise((resolve, reject) => {
+		const controller = new AbortController();
+
+		axios({ ...testAxiosConfig, signal: controller.signal, responseType: 'stream' })
+			.then(resp => {
+				resp.data.on('data', (chunk: any) => {
+					const contentRange = resp.headers['content-range']; // bytes 0-0/104857607
+					const contentLength: 1 | '1' | undefined = resp.headers['content-length'];
+					if (contentLength == 1 && contentRange) {
+						resolve(Number(contentRange.split('/')[1]));
+					} else {
+						resolve(null);
+					}
+					controller.abort();
+				});
+			})
+			.catch(() => {
+				resolve(null);
+			});
+	});
+}
 
 setTimeout(() => {}, 1000000);
