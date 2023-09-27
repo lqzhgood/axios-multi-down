01ed3ac533cd9aa9afd8e6ec9c2bc752eede5ad6
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-09-27 09:31:14 +0800

    🧪 test(down): add .json test

--- test/axios.test.ts
+++ test/axios.test.ts
@@ -1,5 +1,7 @@
 import * as http from 'http';
+import * as fs from 'fs';
 import axiosBase from 'axios';
+import _ from 'lodash';
 
 import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
 
@@ -15,6 +17,9 @@ let server: http.Server;
 
 beforeAll(async () => {
 	server = await testServer();
+	if (!fs.existsSync('../test/files')) {
+		fs.mkdirSync('../test/files');
+	}
 });
 
 afterAll(() => {
@@ -25,7 +30,7 @@ describe('Range is support', () => {
 	const axios = axiosBase.create({});
 	axiosMultiDown(axios);
 
-	test('file is same as ajax', async () => {
+	test('big file is ok', async () => {
 		const f = testFile('100M.test');
 		const url = testUrl('100M.test');
 		makeBigFile(f, 100 * 1024 * 1024);
@@ -33,5 +38,24 @@ describe('Range is support', () => {
 		const _md5File = await md5File(f);
 		const _md5String = md5String(data);
 		expect(_md5File === _md5String).toBe(true);
+	});
+
+	test('json is ok', async () => {
+		const f = testFile('sample.json');
+		const url = testUrl('sample.json');
+		const o = {
+			a: 123,
+			b: true,
+			c: 'string',
+			d: ['1', 123, true],
+			e: {
+				a: 123,
+				b: true,
+				c: 'string',
+			},
+		};
+		fs.writeFileSync(f, JSON.stringify(o));
+		const data = await axios.down({ url }); // default  responseType: 'json'
+		expect(_.isEqual(o, data)).toBe(true);
 	});
 });
