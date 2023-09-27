9abc20af52387951d3d31fa752c5e5c9679431d5
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-09-27 16:10:45 +0800

    🎈 perf(down):
    downOptions 探测方法为可选 默认 HEAD

--- src/types/axios-down.d.ts
+++ src/types/axios-down.d.ts
@@ -1,15 +1,16 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 interface IDownOptions {
 	max: number;
-	testMethod: TEST_METHOD.HEAD | TEST_METHOD.SELF;
+	testMethod?: TEST_METHOD.HEAD | TEST_METHOD.SELF;
 }
 
 type testContentLength = number | null;
 
 // TODO return is not AxiosResponse
 interface AxiosDownMethod {
-	<T = any, R = AxiosResponse<T>, D = any>(configOrUrl: string | AxiosRequestConfig<D>, config?: AxiosRequestConfig<D>): Promise<string>;
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	<T = unknown, R = AxiosResponse<T>, D = unknown>(configOrUrl: string | AxiosRequestConfig<D>, config?: AxiosRequestConfig<D>): Promise<string>;
 }
 
 declare module 'axios' {
