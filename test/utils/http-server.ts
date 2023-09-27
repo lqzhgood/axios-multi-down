090ea1e1e2080ebdb9eada324ed62e60046c5053
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-09-27 10:07:24 +0800

    🧪 test(http-server):
    support query range=no

--- test/utils/http-server.ts
+++ test/utils/http-server.ts
@@ -4,11 +4,13 @@ import * as path from 'path';
 
 const DIR_PUBLIC = '../../';
 
-export function testServer(): Promise<http.Server> {
+export function testServer(port: number): Promise<http.Server> {
 	return new Promise(resolve => {
 		const s = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+			const u = new URL(req.url || '', `http://127.0.0.1:${port}`);
+
 			// 获取请求的文件路径
-			const filePath: string = path.join(__dirname, DIR_PUBLIC, req.url || '');
+			const filePath: string = path.join(__dirname, DIR_PUBLIC, u.pathname || '');
 
 			// 读取文件
 			fs.readFile(filePath, (err: NodeJS.ErrnoException | null, data: Buffer) => {
@@ -23,12 +25,11 @@ export function testServer(): Promise<http.Server> {
 						res.end('Internal Server Error');
 					}
 				} else {
-					// 设置响应头，支持Range属性
-					res.setHeader('Accept-Ranges', 'bytes');
-
 					// 如果请求包含Range头，则处理部分内容请求
 					const range: string | undefined = req.headers.range;
-					if (range) {
+					if (range && u.searchParams.get('range') !== 'no') {
+						// 设置响应头，支持Range属性
+						res.setHeader('Accept-Ranges', 'bytes');
 						const [start, end] = range.replace('bytes=', '').split('-') as [string, string];
 						const totalLength: number = data.length;
 						const partialData: Buffer = data.subarray(parseInt(start), parseInt(end) + 1);
@@ -47,10 +48,8 @@ export function testServer(): Promise<http.Server> {
 			});
 		});
 
-		const PORT: number = 3000;
-
-		s.listen(PORT, () => {
-			console.log(`Server is running on port ${PORT}`);
+		s.listen(port, () => {
+			console.log(`Server is running on port ${port}`);
 			resolve(s);
 		});
 	});
